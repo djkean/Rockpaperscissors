0e8eddcd0dc7306b7519f9e3f9e8c97bf77b7e08
djkean <djkean7@gmail.com>
2022-01-22 18:35:40 -0330

    added local storage for wins/losses

--- rps.js
+++ rps.js
@@ -10,7 +10,7 @@ the way we will have our "computer" pick randomly from rock, paper or scissors *
 btn.addEventListener("click", function() {
     const randomChoiceIndex = getRandomNumber();
     const computerChoice = choice[randomChoiceIndex];
-    console.log('Computer Chose:', computerChoice);
+    console.log("Computer Chose:", computerChoice);
 
 /* this is where we are declaring a variable for our HTML to read for the computer's choice */
 
@@ -46,20 +46,24 @@ for the player's choice */
     {
         gameResult = "Rock beats Scissors, You Win!";
         gameWins ++;
+        localStorage.setItem("games won", gameWins);
     }
     else if (playerChoice == "Paper" && computerChoice != "Scissors")
     {
         gameResult = "Paper beats Rock, You Win!";
         gameWins ++;
+        localStorage.setItem("games won", gameWins);
     }
     else if (playerChoice == "Scissors" && computerChoice != "Rock")
     {
         gameResult = "Scissors beats Paper, You Win!";
         gameWins ++;
+        localStorage.setItem("games won", gameWins);
     }
     else {
         gameResult = "You Lose :(";
         gameLosses ++;
+        localStorage.setItem("games lost", gameLosses);
     }
 
     /* our outcomes are then given variables to be read by the HTML document */
@@ -74,6 +78,22 @@ for the player's choice */
     lossCount.textContent = gameLosses;
     console.log(gameLosses);
 });
+
+window.onload = function()
+{
+gameWins = localStorage.getItem("games won");
+console.log(gameWins);
+gameLosses = localStorage.getItem("games lost");
+console.log(gameLosses);
+
+const winCount = document.getElementById("gameWins");
+winCount.textContent = gameWins;
+console.log(gameWins);
+const lossCount = document.getElementById("gameLosses");
+lossCount.textContent = gameLosses;
+console.log(gameLosses);
+}
+
 
 function getRandomNumber(){
     return Math.floor(Math.random() * choice.length);
