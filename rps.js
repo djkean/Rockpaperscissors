9c68d42ff839d04c6ebd6451c5484988607c618b
djkean <djkean7@gmail.com>
2022-01-31 00:13:49 -0330

    Improved formatting for some mobile devices, implemented gameData object, added img hover effects, implemented <button> for images
    more adjustments to come later

--- rps.js
+++ rps.js
@@ -1,33 +1,52 @@
-let gameWins = 0;
-let gameLosses = 0;
+const gameData = {
+    wins: 0,
+    losses: 0,
+    addWin: () => {
+        gameData.wins++;
+    },
+    addLoss: () => {
+        gameData.losses++;
+    },
+    save: () => {
+        localStorage.setItem("gameData", JSON.stringify(gameData));
+    },
+    load: () => {
+        const getGameData = JSON.parse(localStorage.getItem("gameData"));
+        if (localStorage.getItem("gameData") !== null)
+        {
+            gameData.wins = getGameData.wins;
+            gameData.losses = getGameData.losses;
+        }
+        document.getElementById("gameWins").textContent = gameData.wins;
+        document.getElementById("gameLosses").textContent = gameData.losses;
+    },
+};
 
 const choice = ["Rock", "Paper", "Scissors"];
 const btn = document.getElementById("btn");
 
-/* this is using the logic from the color flipper as the system
-the way we will have our "computer" pick randomly from rock, paper or scissors */
-
+/* our "computer" picks randomly from rock, paper or scissors */
 
 function playerChoice(rpsSelect) {
     const randomChoiceIndex = getRandomNumber();
     const computerChoice = choice[randomChoiceIndex];
     console.log("Computer Chose:", computerChoice); 
 
-/* this is where we are declaring a variable for our HTML to read for the computer's choice */
+/* we declare a variable for our HTML to read for the computer's choice */
 
     const computerOutcome = document.getElementById("computerChoice");
     computerOutcome.textContent = "Computer Choice: " + computerChoice;
     console.log(computerOutcome);
 
-/* we are using radio inputs for the buttons, one for each value
-for the player's choice */
+/* we use "onclick function()" on the images and their values are tied into the "choice" array */
 
     let playerChoice = rpsSelect;
 
     console.log("Player Choice", playerChoice);
     let gameResult;
     
-/* we use if /else if/ else statements to determine the outcome of the game */
+/* we use if /else if/ else statements to determine the outcome of the game,
+then wins/losses are set in localstorage */
 
     if (playerChoice == computerChoice)
     {
@@ -38,58 +57,46 @@ for the player's choice */
     {
         document.getElementById("gameResult").style.background= "#5FB679";
         gameResult = "Rock beats Scissors, You Win!";
-        gameWins ++;
-        localStorage.setItem("games won", gameWins);
+        gameData.addWin();
     }
     else if (playerChoice == "Paper" && computerChoice != "Scissors")
     {
         document.getElementById("gameResult").style.background= "#5FB679";
         gameResult = "Paper beats Rock, You Win!";
-        gameWins ++;
-        localStorage.setItem("games won", gameWins);
+        gameData.addWin();
     }
     else if (playerChoice == "Scissors" && computerChoice != "Rock")
     {
         document.getElementById("gameResult").style.background= "#5FB679";
         gameResult = "Scissors beats Paper, You Win!";
-        gameWins ++;
-        localStorage.setItem("games won", gameWins);
+        gameData.addWin();
     }
     else {
         document.getElementById("gameResult").style.background= "#B0280F";
         gameResult = "You Lose :(";
-        gameLosses ++;
-        localStorage.setItem("games lost", gameLosses);
+        gameData.addLoss();
     }
 
-    /* our outcomes are then given variables to be read by the HTML document */
+    gameData.save();
+
+    /* our outcomes are given variables to be read by the HTML document */
 
     const outcome = document.getElementById("gameResult");
     outcome.textContent = gameResult;
-    console.log(gameResult);
-    const winCount = document.getElementById("gameWins");
-    winCount.textContent = gameWins;
-    console.log(gameWins);
-    const lossCount = document.getElementById("gameLosses");
-    lossCount.textContent = gameLosses;
-    console.log(gameLosses);
+    console.log({gameResult});
+    document.getElementById("gameWins").textContent = gameData.wins;
+    console.log({gameWins});
+    document.getElementById("gameLosses").textContent = gameData.losses;
+    console.log({gameLosses});
 }
+
+/* this retrieves the user's win/loss from localstorage */
 
 window.onload = function()
 {
-gameWins = localStorage.getItem("games won");
-console.log(gameWins);
-gameLosses = localStorage.getItem("games lost");
-console.log(gameLosses);
-
-const winCount = document.getElementById("gameWins");
-winCount.textContent = gameWins;
-console.log(gameWins);
-const lossCount = document.getElementById("gameLosses");
-lossCount.textContent = gameLosses;
-console.log(gameLosses);
+    gameData.load();
 }
-
+/* this is the function used for computer choice */
 
 function getRandomNumber(){
     return Math.floor(Math.random() * choice.length);
