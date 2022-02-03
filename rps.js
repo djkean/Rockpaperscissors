b6394d5aef8aa708b6e43e377b6ae6be34ba492b
djkean <djkean7@gmail.com>
2022-02-03 01:24:13 -0330

    "Page 2" Implemented, Added images for all outcomes.

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+/* localstorage with an object */
+
 const gameData = {
     wins: 0,
     losses: 0,
@@ -25,20 +27,14 @@ const gameData = {
 const choice = ["Rock", "Paper", "Scissors"];
 const btn = document.getElementById("btn");
 
-/* our "computer" picks randomly from rock, paper or scissors */
-
 function playerChoice(rpsSelect) {
     const randomChoiceIndex = getRandomNumber();
     const computerChoice = choice[randomChoiceIndex];
     console.log("Computer Chose:", computerChoice); 
 
-/* we declare a variable for our HTML to read for the computer's choice */
-
     const computerOutcome = document.getElementById("computerChoice");
     computerOutcome.textContent = "Computer Choice: " + computerChoice;
     console.log(computerOutcome);
-
-/* we use "onclick function()" on the images and their values are tied into the "choice" array */
 
     let playerChoice = rpsSelect;
 
@@ -51,52 +47,89 @@ then wins/losses are set in localstorage */
     if (playerChoice == computerChoice)
     {
         document.getElementById("gameResult").style.background= "#6199E9";
+        document.getElementById(playerChoice + "Tie").style.display= "block";
+        document.getElementById("parentOutcomeImage").style.display= "flex";
         gameResult = "Tie";
     } 
     else if (playerChoice == "Rock" && computerChoice != "Paper")
     {
         document.getElementById("gameResult").style.background= "#5FB679";
+        document.getElementById("rockbeatsscissorsWin").style.display= "block";
+        document.getElementById("parentOutcomeImage").style.display= "flex";
         gameResult = "Rock beats Scissors, You Win!";
         gameData.addWin();
     }
     else if (playerChoice == "Paper" && computerChoice != "Scissors")
     {
         document.getElementById("gameResult").style.background= "#5FB679";
+        document.getElementById("paperbeatsrockWin").style.display= "block";
+        document.getElementById("parentOutcomeImage").style.display= "flex";
         gameResult = "Paper beats Rock, You Win!";
         gameData.addWin();
     }
     else if (playerChoice == "Scissors" && computerChoice != "Rock")
     {
         document.getElementById("gameResult").style.background= "#5FB679";
+        document.getElementById("scissorsbeatspaperWin").style.display= "block";
+        document.getElementById("parentOutcomeImage").style.display= "flex";
         gameResult = "Scissors beats Paper, You Win!";
         gameData.addWin();
     }
+    else if (playerChoice == "Rock" && computerChoice == "Paper")
+    {
+        document.getElementById("gameResult").style.background= "#B0280F";
+        document.getElementById("paperbeatsrockLoss").style.display= "block";
+        document.getElementById("parentOutcomeImage").style.display= "flex";
+        gameResult = "Paper beats Rock, You Lose! :(";
+        gameData.addLoss();
+    }
+    else if (playerChoice == "Paper" && computerChoice == "Scissors")
+    {
+        document.getElementById("gameResult").style.background= "#B0280F";
+        document.getElementById("scissorsbeatspaperLoss").style.display= "block";
+        document.getElementById("parentOutcomeImage").style.display= "flex";
+        gameResult = "Scissors beats Paper, You Lose :(";
+        gameData.addLoss();
+    }
     else {
         document.getElementById("gameResult").style.background= "#B0280F";
-        gameResult = "You Lose :(";
+        document.getElementById("rockbeatsscissorsLoss").style.display= "block";
+        document.getElementById("parentOutcomeImage").style.display= "flex";
+        gameResult = "Rock beats Scissors, You Lose :(";
         gameData.addLoss();
     }
 
     gameData.save();
 
-    /* our outcomes are given variables to be read by the HTML document */
-
     const outcome = document.getElementById("gameResult");
     outcome.textContent = gameResult;
-    console.log({gameResult});
+    console.log(gameResult);
     document.getElementById("gameWins").textContent = gameData.wins;
-    console.log({gameWins});
+    console.log(gameWins);
     document.getElementById("gameLosses").textContent = gameData.losses;
-    console.log({gameLosses});
+    console.log(gameLosses);
 }
+ 
+/* a whole mess of stuff for hiding things for "page2" */
 
-/* this retrieves the user's win/loss from localstorage */
+document.getElementById("replayButton").addEventListener("click", () => {
+    document.getElementById("parentOutcomeImage").style.display= "none";
+    document.getElementById("rockbeatsscissorsWin").style.display= "none";
+    document.getElementById("rockbeatsscissorsLoss").style.display= "none";
+    document.getElementById("paperbeatsrockWin").style.display= "none";
+    document.getElementById("paperbeatsrockLoss").style.display= "none";
+    document.getElementById("scissorsbeatspaperWin").style.display= "none";
+    document.getElementById("scissorsbeatspaperLoss").style.display= "none";
+    document.getElementById("RockTie").style.display= "none";
+    document.getElementById("PaperTie").style.display= "none";
+    document.getElementById("ScissorsTie").style.display= "none";
+});
 
 window.onload = function()
 {
     gameData.load();
+    document.getElementById("parentOutcomeImage").style.display= "none";
 }
-/* this is the function used for computer choice */
 
 function getRandomNumber(){
     return Math.floor(Math.random() * choice.length);
